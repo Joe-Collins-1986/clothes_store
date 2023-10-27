43efabb6a677f350a23f5f0d2d0ee6d4bac2b9b7
Joseph Collins <joecollins@Josephs-MBP.home>
2023-10-27 12:24:48 +0100

    create react app with basic directory

---
+++ src/components/category-item/CatagoryItem.jsx
@@ -0,0 +1,22 @@
+import "./category-item-styles.scss";
+
+import React from "react";
+
+const CatagoryItem = ({ category }) => {
+  const { imageUrl, title } = category;
+
+  return (
+    <div className="category-container">
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+};
+
+export default CatagoryItem;
