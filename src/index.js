a81976221709524bb400ad693995bdfcd29b8f56
Joseph Collins <joecollins@Josephs-MBP.home>
2023-11-17 17:13:02 +0000

    add cart context to track dropdown toggle

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import "./index.scss";
 import App from "./App";
 import { UserProvider } from "./contexts/user.context";
 import { ProductsProvider } from "./contexts/products.context";
+import { CartProvider } from "./contexts/cart.context";
 
 import reportWebVitals from "./reportWebVitals";
 
@@ -17,7 +18,9 @@ root.render(
     <BrowserRouter>
       <UserProvider>
         <ProductsProvider>
-          <App />
+          <CartProvider>
+            <App />
+          </CartProvider>
         </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
