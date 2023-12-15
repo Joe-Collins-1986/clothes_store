72bdcdb3c25f5833e65ef4f182987a5ffa7f7a4a
Joseph Collins <joecollins@Josephs-MBP.home>
2023-12-15 10:15:04 +0000

    update sytle on cart-item, catagory-preview and input-form

--- src/components/catagory-preview/CatagoryPreview.jsx
+++ src/components/catagory-preview/CatagoryPreview.jsx
@@ -1,26 +1,26 @@
-import { Link } from "react-router-dom";
+import ProductCard from "../product-card/ProductCard";
 
-import ProductCard from "../../components/product-card/ProductCard";
+import {
+  CategoryPreviewContainer,
+  Title,
+  Preview,
+} from "./category-preview.styles";
 
-import "./category-preview.styles.scss";
-
-const CatagoryPreview = ({ title, products }) => {
+const CategoryPreview = ({ title, products }) => {
   return (
-    <div className="category-preview-container ">
+    <CategoryPreviewContainer>
       <h2>
-        <Link className="title" to={title}>
-          {title.toUpperCase()}
-        </Link>
+        <Title to={title}>{title.toUpperCase()}</Title>
       </h2>
-      <div className="preview">
+      <Preview>
         {products
           .filter((_, idx) => idx < 4)
           .map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
-      </div>
-    </div>
+      </Preview>
+    </CategoryPreviewContainer>
   );
 };
 
-export default CatagoryPreview;
+export default CategoryPreview;
