c53e390dee9fcd233421ba706163545c816f1be3
Joesph Collins <collins.jw@btinternet.com>
2024-02-13 14:38:59 +0000

    manage paymentIntent

--- src/components/payment-form/PaymentForm.jsx
+++ src/components/payment-form/PaymentForm.jsx
@@ -23,17 +23,39 @@ const PaymentForm = () => {
       body: JSON.stringify({ amount: 1000 }),
     }).then((res) => res.json());
 
-    console.log(response);
+    const {
+      paymentIntent: { client_secret },
+    } = response;
+
+    console.log(client_secret);
+
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: "Joe Collins",
+        },
+      },
+    });
+
+    if (paymentResult.error) {
+      alert(paymentResult.error.message);
+    } else {
+      if (paymentResult.paymentIntent.status === "succeeded") {
+        alert("Payment successful");
+      }
+    }
+
+    return (
+      <PaymentFormContainer>
+        <FormContainer onSubmit={paymentHandler}>
+          <h2>Credit Card Payment: </h2>
+          <CardElement />
+          <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</Button>
+        </FormContainer>
+      </PaymentFormContainer>
+    );
   };
-  return (
-    <PaymentFormContainer>
-      <FormContainer onSubmit={paymentHandler}>
-        <h2>Credit Card Payment: </h2>
-        <CardElement />
-        <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</Button>
-      </FormContainer>
-    </PaymentFormContainer>
-  );
 };
 
 export default PaymentForm;
